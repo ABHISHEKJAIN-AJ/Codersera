4c0a2d45d840aeba4414cc56d35ef183b79ef228
Abhi <abhi@apples-MacBook-Pro-8.local>
2020-01-13 10:13:12 +0000

    code refactor

---
+++ components/aboutprimaryheader.js
@@ -0,0 +1,9 @@
+const AboutPrimaryHeader = () => (
+    <div className="about-primary-header">
+        <div className="about-primary-header-content">
+            <h2>About us…</h2>
+            <p>Learn how Codersera selects the best of the best coders through a thorough and detailed analysis in a 5 Tire Process.</p>
+        </div>
+    </div>
+)
+export default AboutPrimaryHeader;
---
+++ pages/about.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Head from 'next/head'
+import Nav from '../components/nav'
+import '../scss/main.scss'
+import Footer from "../components/footer";
+import AboutPrimaryHeader from "../components/aboutprimaryheader";
+
+
+
+const About = () => (
+    <div className="app-about">
+        <Head>
+            <title>About Us</title>
+            <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans|Raleway|Rubik&display=swap" rel="stylesheet"/>
+        </Head>
+
+        <Nav/>
+<AboutPrimaryHeader/>
+        <Footer/>
+
+    </div>
+)
+
+export default About;
